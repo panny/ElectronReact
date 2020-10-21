d69d3f16af01e93617c709a1a9fa0ce714129a55
Yeung Panny <panny.yeung@autotron.io>
2020-10-22 07:10:44 +0800

    Webpack Installation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Main Process
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification } = require('electron');
+const path = require('path');
 
 function createWindow(){
     // Browser Window <- Renderer Process
@@ -7,7 +8,13 @@ function createWindow(){
         width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false,
+            // will sanitize JS code
+            // TODO: explain when React application is initialize
+            worldSafeExecuteJavaScript: true,
+            // is a feature that ensures that both, your preload scripts and 
+            // Electrons internal logic tun in separate context
+            contextIsolation: true
         }
     })
     win.loadFile('index.html')
@@ -16,16 +23,17 @@ function createWindow(){
 
 app.whenReady().then(createWindow);
 
+// For Dawin
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin'){
         app.quit();
     }
 })
 
-// For Darwin
 app.on('activate', () => {
     if(BrowserWindow.getAllWindows().length === 0){
         createWindow();
     }
 })
 
+// Chromium -> web engine for rendering the UI, full Chrome-like web browser
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from ' react-dom';
+
+ReactDOM.render(<h1>I am React App!</h1>, document.getElementsById('electronChat'));
