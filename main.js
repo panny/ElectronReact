e4bba8e0e3132fa4651362fa3bca0de95b1c878a
Yeung Panny <panny.yeung@autotron.io>
2020-10-21 14:40:54 +0800

    Initialize Electron

---
+++ main.js
@@ -0,0 +1,31 @@
+// Main Process
+const { app, BrowserWindow } = require('electron');
+
+function createWindow(){
+    // Browser Window <- Renderer Process
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('index.html')
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+
+// For Darwin
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow();
+    }
+})
+
